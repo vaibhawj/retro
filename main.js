5dbeded77f29c8f8b4e7fcb7c252cd9cdf2c6d3f
Arbin <arbin.cheng@philips.com>
2019-05-17 15:59:41 +0800

    Updates from 079c20e Arbin remove boardContext, add author and options to hide voter

--- main.js
+++ main.js
@@ -177,6 +177,9 @@ angular.module('fireideaz').controller('MainCtrl', ['$cookies', '$scope', '$filt
         date_created: Date.now(),
         columns: $scope.messageTypes,
         user: $scope.user,
+        hidevoter:true,
+        hideAuthor:true,
+        hidevote:false,
         text_editing_is_private: $scope.newBoard.text_editing_is_private
       }).then(function (result) {
         console.log(result);
@@ -408,13 +411,13 @@ angular.module('fireideaz').controller('MessageCtrl', ['$scope', '$window', 'Mod
 'use strict';
 
 angular
-  .module('fireideaz')
-  .service('Utils', [function () {
+  .module('fireideaz').service('Utils', [function () {
     function createUserId() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
-            return v.toString(16);
-        });
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+          v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
     }
 
     function focusElement(id) {
@@ -465,7 +468,88 @@ angular
       columnClass: columnClass
     };
   }]);
+'use strict';
 
+angular
+  .module('fireideaz')
+  .service('ModalService', ['ngDialog', function (ngDialog) {
+    return {
+      openLoginUser: function (scope) {
+        ngDialog.open({
+          template: 'components/setUserName.html',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      openAddNewColumn: function (scope) {
+        ngDialog.open({
+          template: 'addNewColumn',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      openAddNewBoard: function (scope) {
+        ngDialog.open({
+          template: 'addNewBoard',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      openDeleteCard: function (scope) {
+        ngDialog.open({
+          template: 'deleteCard',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      openDeleteColumn: function (scope) {
+        ngDialog.open({
+          template: 'deleteColumn',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+
+      openMergeCards: function (scope) {
+        ngDialog.open({
+          template: 'mergeCards',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      openDeleteBoard: function (scope) {
+        ngDialog.open({
+          template: 'deleteBoard',
+          className: 'ngdialog-theme-plain danger',
+          scope: scope
+        });
+      },
+      openDeleteCards: function (scope) {
+        ngDialog.open({
+          template: 'deleteCards',
+          className: 'ngdialog-theme-plain danger',
+          scope: scope
+        });
+      },
+      openVoteSettings: function (scope) {
+        ngDialog.open({
+          template: 'voteSettings',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      openCardSettings: function (scope) {
+        ngDialog.open({
+          template: 'cardSettings',
+          className: 'ngdialog-theme-plain',
+          scope: scope
+        });
+      },
+      closeAll: function () {
+        ngDialog.closeAll();
+      }
+    };
+  }]);
 'use strict';
 
 angular.module('fireideaz').directive('about', [function() {
@@ -589,86 +673,3 @@ angular.module('fireideaz').directive('spinner', [function() {
     };
   }]
 );
-
-'use strict';
-
-angular
-  .module('fireideaz')
-  .service('ModalService', ['ngDialog', function (ngDialog) {
-    return {
-      openLoginUser: function (scope) {
-        ngDialog.open({
-          template: 'components/setUserName.html',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      openAddNewColumn: function (scope) {
-        ngDialog.open({
-          template: 'addNewColumn',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      openAddNewBoard: function (scope) {
-        ngDialog.open({
-          template: 'addNewBoard',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      openDeleteCard: function (scope) {
-        ngDialog.open({
-          template: 'deleteCard',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      openDeleteColumn: function (scope) {
-        ngDialog.open({
-          template: 'deleteColumn',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-
-      openMergeCards: function (scope) {
-        ngDialog.open({
-          template: 'mergeCards',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      openDeleteBoard: function (scope) {
-        ngDialog.open({
-          template: 'deleteBoard',
-          className: 'ngdialog-theme-plain danger',
-          scope: scope
-        });
-      },
-      openDeleteCards: function (scope) {
-        ngDialog.open({
-          template: 'deleteCards',
-          className: 'ngdialog-theme-plain danger',
-          scope: scope
-        });
-      },
-      openVoteSettings: function (scope) {
-        ngDialog.open({
-          template: 'voteSettings',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      openCardSettings: function (scope) {
-        ngDialog.open({
-          template: 'cardSettings',
-          className: 'ngdialog-theme-plain',
-          scope: scope
-        });
-      },
-      closeAll: function () {
-        ngDialog.closeAll();
-      }
-    };
-  }]);
